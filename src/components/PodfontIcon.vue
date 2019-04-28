d27a746b0232f7e78ac6d64ececd85ba38ac901d
Brandon Lyons <lyonsbp@umich.edu>
2019-04-27 22:53:55 -0400

    Added PodfontIcon component

---
+++ src/components/PodfontIcon.vue
@@ -0,0 +1,55 @@
+<template>
+  <div v-if="svg" v-html="svg" :style="cssProps" :class="size"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: '#00274c'
+    },
+    icon: {},
+    size: {
+      type: String,
+      default: 'md'
+    }
+  },
+  data () {
+    return {
+      svg: null
+    }
+  },
+  computed: {
+    cssProps () {
+      return {
+        '--fill-color': this.color
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+div {
+  color: var(--fill-color);
+  .xxs {
+    width: 0.85em;
+  }
+  .xs {
+    width: 1em;
+  }
+  .sm {
+    width: 1.5em;
+  }
+  .md {
+    width: 2em;
+  }
+  .lg {
+    width: 3em;
+  }
+  .xl {
+    width: 4em;
+  }
+}
+</style>
